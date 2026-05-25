0afb5d5759f1339f73bfebb5e0e24d4352e0fbcf
Rob Curcio <guitarist4life92@gmail.com>
2026-05-25 13:30:48 -0400

    Simple circle render

--- src/vec3.h
+++ src/vec3.h
@@ -8,6 +8,7 @@ class vec3
 {
 public:
     double e[3];
+
     vec3() : e{0, 0, 0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
@@ -70,6 +71,11 @@ inline vec3 operator-(const vec3 &u, const vec3 &v)
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
 inline vec3 operator*(double t, const vec3 &v)
 {
     return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
@@ -87,7 +93,7 @@ inline vec3 operator/(const vec3 &v, double t)
 
 inline double dot(const vec3 &u, const vec3 &v)
 {
-    return u.e[0] * v.e[0] + u.e[1] + v.e[1] + u.e[2] * v.e[2];
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
 }
 
 inline vec3 cross(const vec3 &u, const vec3 &v)
